00d9ea3bf741730728e0039e0e98867583ceccfb
Devashish Tripathi <151374775+Devashish-Tripathi@users.noreply.github.com>
2026-07-29 10:34:26 +0530

    Add: design-hashmap - submission-1

---
+++ Data Structures & Algorithms/design-hashmap/submission-1.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, key = None, value = None, next= None):
+        self.key = key
+        self.value = value
+        self.next = next
+
+class MyHashMap:
+
+    def __init__(self):
+        self.obj = [Node() for _ in range(1000)]
+    
+    def _hash(self, key):
+        return key % 1000
+
+    def put(self, key: int, value: int) -> None:
+        curr = self.obj[self._hash(key)]
+        while curr.next:
+            if curr.next.key == key:
+                curr.next.value = value
+                return
+            curr = curr.next
+        curr.next = Node(key, value)
+
+    def get(self, key: int) -> int:
+        curr = self.obj[self._hash(key)].next
+        while curr:
+            if curr.key == key:
+                return curr.value
+            curr = curr.next
+        return -1
+
+    def remove(self, key: int) -> None:
+        curr = self.obj[self._hash(key)]
+        while curr.next:
+            if curr.next.key == key:
+                curr.next = curr.next.next
+                return
+            curr = curr.next
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
