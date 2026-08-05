12003e8062a9f5ab6953b765184b4f6d2262c24f
Devashish Tripathi <151374775+Devashish-Tripathi@users.noreply.github.com>
2026-08-05 12:58:31 +0530

    Add: path-with-minimum-effort - submission-3

---
+++ Data Structures & Algorithms/path-with-minimum-effort/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        visited = {}
+        travels = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        m, n = len(heights), len(heights[0])
+        rng_r, rng_c = range(m), range(n)
+        heap = [(0, 0, 0)] # mad, x, y
+
+        while heap:
+            mad, x, y = heapq.heappop(heap)
+            if (x, y) in visited:
+                # important for dijisktra's. the monotonic nature ensures first pop is the best distance we can get to a node
+                continue
+            if x==m-1 and y==n-1:
+                return mad
+            visited[(x, y)] = mad
+            for dx, dy in travels:
+                nx, ny = x+dx, y+dy
+                if nx in rng_r and ny in rng_c:
+                    nx_mad = max(mad, abs(heights[nx][ny]-heights[x][y]))
+                    if (nx, ny) not in visited or visited[(nx, ny)] > mad:
+                        heapq.heappush(heap, (nx_mad, nx, ny))
+        print(visited)
+        return visited[(m-1, n-1)]
