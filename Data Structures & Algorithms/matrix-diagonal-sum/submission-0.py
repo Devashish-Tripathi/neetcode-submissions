bc7d73e433c51d66b48fd1ab94901ae88cbbfb95
Devashish Tripathi <151374775+Devashish-Tripathi@users.noreply.github.com>
2026-09-22 10:45:32 +0530

    Add: matrix-diagonal-sum - submission-0

---
+++ Data Structures & Algorithms/matrix-diagonal-sum/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        n = len(mat)
+        ans = 0
+        for i in range(n):
+            ans += mat[i][i]
+        for i in range(n):
+            ans += mat[i][n-i-1]
+        if n % 2:
+            ans -= mat[n//2][n//2]
+        return ans
